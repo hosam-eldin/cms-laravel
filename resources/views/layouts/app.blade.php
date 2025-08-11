0669e4e9fc0f59e5a9cf893c9ca61cecbcf7d0be
hosam-eldin <h.eldinmoh95@gmail.com>
2025-08-11 19:14:35 +0300

    add-post-and-category-models

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,7 +76,30 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            @Auth
+                <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                         <ul class="list-group">
+                             <li class="list-group-item">
+                                <a href="">posts</a>
+                            </li>
+                            <li class="list-group-item">
+                                <a href="">categories</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-8">
+                        @yield('content')
+                    </div>
+                </div>
+               
+            </div>
+            @else
+                 @yield('content')
+            @endAuth
+
+            
         </main>
     </div>
 </body>
