5264bbe66cf8eb0008d70493e5d8dbed57912118
hosam-eldin <h.eldinmoh95@gmail.com>
2025-08-15 00:19:44 +0300

    display posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,33 @@
    <div class="card card-default">
       <div class="card-header">Posts</div>
       <div class="card-body">
+         <table class="table">
+            <thead>
+               <th>image</th>
+               <th>title</th>
+               <th></th>
+               <th></th>
 
+            </thead>
+            <tbody>
+               @foreach ($posts as $post)
+                  <tr>
+                     <td>
+                        <img src="{{ asset('storage/' . $post->image) }}" width="120px" height="60px" alt="">
+                     </td>
+                     <td>
+                        {{ $post->title }}
+                     </td>
+                     <td>
+                        <a href="{{ route('posts.show', $post->id) }}" class="btn btn-info btn-small">Edit</a>
+                     </td>
+                     <td>
+                        <a href="{{ route('posts.destroy', $post->id) }}" class="btn btn-danger btn-small">Trash</a>
+                     </td>
+                  </tr>
+               @endforeach
+            </tbody>
+         </table>
       </div>
    </div>
 @endsection
