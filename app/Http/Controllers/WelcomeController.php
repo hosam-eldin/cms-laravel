c8bcb8d0e5340f74cbcfd19410d17f369f0fe397
hosam-eldin <h.eldinmoh95@gmail.com>
2025-08-19 18:40:44 +0300

    paginate posts pages

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -15,6 +15,6 @@ class WelcomeController extends Controller
         return view('welcome')
             ->with('categories', Category::all())
             ->with('tags', Tag::all())
-            ->with('posts', Post::all());
+            ->with('posts', Post::simplepaginate(1));
     }
 }
